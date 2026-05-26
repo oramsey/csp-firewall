b38baf32b49a6b288ae7e2859fbd14d52e24a0ba
Owen Ramsey <oramsey004@gmail.com>
2026-05-26 23:21:50 +0000

    Container number 4 only sets up a malicious ground node no ZMQ hub
    configured as node 11 to differentiate it from the benign node which is node 10
    
    in the docker/maliciou_node directory which has a dockerfile and an entrypoint.sh

---
+++ docker/malicious_node/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Start Malicious Ground Node (Node 11)
+# It connects to the hub specified during build (ground-hub-a)
+echo "Starting Malicious Ground Node..."
+cd /suchai/build/apps/simple
+./suchai-app
